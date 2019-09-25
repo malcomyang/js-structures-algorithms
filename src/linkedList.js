e7c17ca328556c1a614851395b5d37257888294f
Malcom Yang <hnkdyangqi@qq.com>
2019-09-25 19:08:34 +0800

    update linkedList

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,27 +1,27 @@
 class Node{
     constructor(element){
+        if (element == null && element == undefined){
+            throw new Error('Nodes cannot be null or undefined.');
+        }
         this.element = element;
         this.next = null;
         this.previous = null;
     }
 }
 
+const ele = Symbol('element');
+const head = Symbol('head');
 class LinkedList {
     constructor (element) {
-        this.head = new Node('head');
+        this[head] = new Node(ele);
         if (element != null && element != undefined){
-            let newNode = new Node(element);
-            newNode.previous = this.head;
-            this.head.next = newNode;
+            this[head].next = new Node(element);
+            this[head].next.previous = this[head];
         }
     }
 
     find (item) {
-        if (item == null || item == undefined){
-            throw new Error('Can not opearation null or undefined.');
-        }
-
-        let currNode = this.head;
+        let currNode = this[head];
         while (currNode.element != item && currNode.next != null){
             currNode = currNode.next;
         }
@@ -31,27 +31,39 @@ class LinkedList {
         return null;
     }
 
+    first () {
+        return this[head].next;
+    }
+
+    last () {
+        let currNode = this[head];
+        while(currNode.next !== null){
+            currNode = currNode.next;
+        }
+        return currNode;
+    }
+
     insert (item, element) {
         let current = find(item);
-        if (current == null) throw new Error('');
-        if (element == null || element == undefined) throw new Error('Can not insert null or undefined.');
+        if (current == null) throw new Error('The specified node is not in the list.');
 
         let newNode = new Node(element);
         newNode.previous = current;
         newNode.next = newNode.next
-        newNode.next.previous = newNode
+        if (current.next != null) {
+            current.next.previous = newNode
+        }
         current.next = newNode;
     }
 
     remove (item) {
         let currNode = find(item);
         if (currNode == null){
-            console.warn('');
             return;
         }
         
         currNode.previous.next = currNode.next;
-        currNode.next.previous = currNode.previous;
+        if (currNode.next != null) currNode.next.previous = currNode.previous;
         currNode = null;
     }
 }
